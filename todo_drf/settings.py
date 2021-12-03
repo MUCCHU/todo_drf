80868f1019d9cd5040e54d3471fe60ab4d5e52a4
Harsh Wasnik <harshwasnik160602@gmail.com>
2021-12-03 12:24:16 +0530

    static root configured and whitenoise middleware added

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -53,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 ROOT_URLCONF = 'todo_drf.urls'
 
 TEMPLATES = [
